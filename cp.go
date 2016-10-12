dc4ade8f931017fbe2aa083abb6218e2f9271d66
puslip41 <puslip41@gmail.com>
2016-10-12 22:29:52 +0900

    function rename

--- cp.go
+++ cp.go
@@ -6,43 +6,41 @@ import (
 	"io"
 )
 
-func checkArgs(args []string) (bool, string, string, string) {
-	isStart := false
-	var command string
-	var sourceFileName string
-	var destinationFileName string
+func getCpArgs(args []string) (bool, string, string, string) {
+	isValid := false
+	var command, sourceFileName, destinationFileName string
 
 	if len(args) == 1 {
-		fmt.Printf("%s: missing file operand", args[0] )
+		fmt.Println("%s: missing file operand", args[0] )
 		os.Exit(-1)
 	} else if len(args) == 2 {
-		fmt.Printf("%s: missing destination file operand after '%s'", args[0], args[1])
+		fmt.Println("%s: missing destination file operand after '%s'", args[0], args[1])
 		os.Exit(-1)
 	} else if len(args) == 3 {
-		isStart = true
+		isValid = true
 
 		command = args[0]
 		sourceFileName = args[1]
 		destinationFileName = args[2]
 	}
 
-	return isStart, command, sourceFileName, destinationFileName
+	return isValid, command, sourceFileName, destinationFileName
 }
 
 func main() {
-	isStart, command, sourceFileName, destinationFileName := checkArgs(os.Args)
+	isValidArgs, command, sourceFileName, destinationFileName := getCpArgs(os.Args)
 
-	if isStart {
+	if isValidArgs {
 		fi, err := os.Open(sourceFileName)
 		if err != nil {
-			fmt.Printf("%s: cannot stat '%s'", command, sourceFileName)
+			fmt.Println("%s: cannot stat '%s'\n%s", command, sourceFileName, err.Error())
 			os.Exit(-1)
 		}
 		defer fi.Close()
 
 		fo, err := os.Create(destinationFileName)
 		if err != nil {
-			fmt.Printf("%s: cannot stat '%s'", command, destinationFileName)
+			fmt.Println("%s: cannot stat '%s'", command, destinationFileName)
 			os.Exit(-1)
 		}
 		defer fo.Close()
@@ -52,7 +50,7 @@ func main() {
 		for {
 			readCount, err := fi.Read(buffer)
 			if err != nil && err != io.EOF {
-				fmt.Printf("%s: cannot copy file: read error '%s'", command, sourceFileName)
+				fmt.Println("%s: cannot copy file: read error '%s'", command, sourceFileName)
 				os.Exit(-1)
 			}
 
@@ -62,7 +60,7 @@ func main() {
 
 			_, err = fo.Write(buffer[:readCount])
 			if err != nil {
-				fmt.Printf("%s: cannot copy file: write error '%s'", command, destinationFileName)
+				fmt.Println("%s: cannot copy file: write error '%s'", command, destinationFileName)
 			}
 		}
 	}
